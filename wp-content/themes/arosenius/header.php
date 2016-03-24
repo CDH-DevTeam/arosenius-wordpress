d20cc3168c520fd407901934f3d9adc4fb968076
traustid <traustid@gmail.com>
2016-03-24 09:36:17 +0100

    Facebook plugin support.

--- wp-content/themes/arosenius/header.php
+++ wp-content/themes/arosenius/header.php
@@ -22,7 +22,14 @@
 </head>
 
 <body <?php body_class(); ?>>
-
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.5&appId=421230791326053";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <div id="mainHeader" class="header">
 
 	<div class="overlay"></div>
